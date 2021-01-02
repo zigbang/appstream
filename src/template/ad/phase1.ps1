33cc70c7b5a32e73ba59fdbb511db6a744ad6b05
ywchung <ywchung@zigbang.com>
2021-01-02 12:47:07 +0900

    nodejs version 수정

--- src/template/ad/phase1.ps1
+++ src/template/ad/phase1.ps1
@@ -1,7 +1,7 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 choco install -y awscli
-choco install -y nodejs
+choco install -y nodejs-lts
 choco install -y yarn
 
 Set-Location \
